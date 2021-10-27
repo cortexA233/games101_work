cbdd1f64d21def828540f2e6bd657b1a3cd0d1fa
cortexA233 <39519037+cortexA233@users.noreply.github.com>
2021-10-27 16:32:49 +0800

    修改贴图

--- work_3/code/main.cpp
+++ work_3/code/main.cpp
@@ -94,7 +94,7 @@ Eigen::Vector3f texture_fragment_shader(const fragment_shader_payload& payload)
     if (payload.texture)
     {
         // TODO: Get the texture value at the texture coordinates of the current fragment
-
+        return_color += payload.texture->getColor(payload.tex_coords.x(), payload.tex_coords.y());
     }
     Eigen::Vector3f texture_color;
     texture_color << return_color.x(), return_color.y(), return_color.z();
